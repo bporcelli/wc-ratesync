7b537b2bd1016f3adfd7713beafa0b3bf77109de
Brett Porcelli <brettporcelli@gmail.com>
2018-10-27 14:04:21 -0400

    build: implement new build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
-module.exports = function( grunt ) {
-    grunt.initConfig( { 
+module.exports = function (grunt) {
+    var pkg = require('./package.json');
+
+    grunt.initConfig({
         addtextdomain: {
             target: {
                 files: {
@@ -7,7 +9,8 @@ module.exports = function( grunt ) {
                         '*.php',
                         '**/*.php',
                         '!node_modules/**',
-                        '!includes/vendor/**'
+                        '!includes/vendor/**',
+                        '!build/**'
                     ]
                 }
             }
@@ -21,28 +24,55 @@ module.exports = function( grunt ) {
                         'report-msgid-bugs-to': 'https://github.com/bporcelli/wc-ratesync/issues'
                     },
                     type: 'wp-plugin',
-                    exclude: [ 'includes/vendor/.*' ]
+                    exclude: ['includes/vendor/.*', 'build/.*']
                 }
             }
         },
         uglify: {
             target: {
-                files: [ {
+                files: [{
                     expand: true,
                     cwd: 'assets/js',
-                    src: [ '*.js', '!*.min.js' ],
+                    src: ['*.js', '!*.min.js'],
                     dest: 'assets/js',
-                    rename: function ( dst, src ) {
-                        return dst + '/' + src.replace( '.js', '.min.js' );
+                    rename: function (dst, src) {
+                        return dst + '/' + src.replace('.js', '.min.js');
                     }
-                } ]
+                }]
+            }
+        },
+        clean: ['build/'],
+        copy: {
+            target: {
+                expand: true,
+                src: ['assets/**', 'data/**', 'includes/**', 'languages/**', 'wc-ratesync.php'],
+                dest: 'build/'
+            }
+        },
+        compress: {
+            target: {
+                options: {
+                    archive: function () {
+                        return 'releases/wc-ratesync-' + pkg.version + '.zip'
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build/',
+                    src: '**',
+                    dest: 'wc-ratesync/'
+                }]
             }
         }
-    } );
+    });
 
-    grunt.loadNpmTasks( 'grunt-wp-i18n' );
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks('grunt-wp-i18n');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask( 'i18n', [ 'addtextdomain', 'makepot' ] );
-    grunt.registerTask( 'default', [ 'i18n', 'uglify' ] );
+    grunt.registerTask('i18n', ['addtextdomain', 'makepot']);
+    grunt.registerTask('build', ['i18n', 'uglify', 'clean', 'copy', 'compress']);
+    grunt.registerTask('default', ['build']);
 };
